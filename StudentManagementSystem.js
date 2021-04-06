b562ebd0b61c9e1a399721f8e7b3409feec35765
SoumayaLabbene <soumaya.labbene@medtech.tn>
2021-04-06 12:24:43 +0100

    add and get

--- StudentManagementSystem.js
+++ StudentManagementSystem.js
@@ -1,4 +1,8 @@
-function add (number1, number2){
-    return number1 + number2
-};
-add(1,2)
+var numberOfStudents = 0;
+function addStudent(){
+    numberOfStudents++;
+}
+function getNumberOfStudents(){
+    return numberOfStudents;
+}
+
